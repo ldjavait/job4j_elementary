5caff2720331a678f49b7c79be68f1eb30b717f9
D L <lazykindit@yandex.ru>
2023-08-03 12:20:40 +0300

    9.2. Преобразование примитивных типов [#383106  [#383106 #229987]]

---
+++ src/main/java/ru/job4j/Types.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Types {
+    public static void main(String[] args) {
+        double d = 121.19;
+        byte b = (byte) d;
+        System.out.println(b);
+    }
+}
