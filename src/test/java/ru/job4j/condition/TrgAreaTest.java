527e79d55b2a33f93dc49a35df82a7a182b4713d
D L <lazykindit@yandex.ru>
2023-08-04 13:29:03 +0300

    add test for ru.job4j.condition.TrgArea

---
+++ src/test/java/ru/job4j/condition/TrgAreaTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TrgAreaTest {
+    @Test
+    public void when2to2to2Than1dot73() {
+        byte a = 2;
+        byte b = 2;
+        byte c = 2;
+        double expected = 1.73;
+        double result = TrgArea.area(a, b, c);
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
+    }
+}
