b717614b005d56e74e929a3a41f6536aecb0301c
D L <lazykindit@yandex.ru>
2023-08-05 18:53:52 +0300

    10. Похвали себя за проделанную работу

---
+++ src/main/java/ru/job4j/Praise.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+/**
+ * Напишите программу, которая выводит на консоль строчку "Сегодня я молодец!"
+ */
+public class Praise {
+    public static void main(String[] args) {
+        System.out.println("Сегодня я молодец!");
+    }
+}
---
+++ src/test/java/ru/job4j/PraiseTest.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class PraiseTest {
+    @Test
+    public void checkPraise() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Praise.main(null);
+        assertThat(out.toString(), is("Сегодня я молодец!" + System.lineSeparator()));
+    }
+}
