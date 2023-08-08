7181562811ba53f136010bd56f86748c3bcc5db1
D L <lazykindit@yandex.ru>
2023-08-08 12:21:07 +0300

    22. Форматированный вывод

---
+++ src/main/java/ru/job4j/io/Task22.java
@@ -0,0 +1,8 @@
+package ru.job4j.io;
+
+public class Task22 {
+    public static void nameInsert(String name) {
+        String rsl = String.format("Привет, %s!", name);
+        System.out.println(rsl);
+    }
+}
