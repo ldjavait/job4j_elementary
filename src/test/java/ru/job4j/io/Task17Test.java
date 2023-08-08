351cc11272df02f311a3c31a21608935e6c1f5c6
D L <lazykindit@yandex.ru>
2023-08-08 11:02:29 +0300

    17. Вывод на консоль квадрат и куб числа

---
+++ src/test/java/ru/job4j/io/Task17Test.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class Task17Test {
+    @Test
+    public void shouldOutputSqrAndCubeOf6() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task17.main(null);
+        String expected = "6"
+                + System.lineSeparator()
+                + "36.0"
+                + System.lineSeparator()
+                + "216.0" + System.lineSeparator();
+        assertThat(out.toString(), is(expected));
+    }
+}
