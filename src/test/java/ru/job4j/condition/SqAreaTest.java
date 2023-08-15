1246249ebf832914be271baa4f9421bd1c5e5157
D L <lazykindit@yandex.ru>
2023-08-15 10:17:35 +0300

    4.5. Отладка программы в IDEA [#159464 #232650]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -7,10 +7,15 @@ import static org.assertj.core.api.Assertions.*;
 class SqAreaTest {
     @Test
     public void whenP6K2Than2() {
-        byte p = 6;
-        byte k = 2;
         double expected = 2;
-        double result = SqArea.square(p, k);
+        double result = SqArea.square(6, 2);
         assertThat(result).isEqualTo(expected, offset(0.01));
+    }
+
+    @Test
+    public void whenP4K1Than1() {
+        byte expected = 1;
+        double rsl = SqArea.square(4, 1);
+        assertThat(rsl).isCloseTo(expected, offset(0.01));
     }
 }
