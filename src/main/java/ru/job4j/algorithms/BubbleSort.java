192f6de5da3acaadbfe5aec82b0c54ff1876c35e
D L <lazykindit@yandex.ru>
2023-08-20 17:27:25 +0300

    Алгоритм поиска минимального элемента в массиве. Добавил описание.

---
+++ src/main/java/ru/job4j/algorithms/BubbleSort.java
@@ -0,0 +1,41 @@
+package ru.job4j.algorithms;
+
+/**
+ * Алгоритм состоит из повторяющихся проходов по сортируемому массиву.
+ * За каждый проход элементы последовательно сравниваются попарно и,
+ * если порядок в паре неверный, выполняется перестановка элементов.
+ * Проходы по массиву повторяются до тех пор,
+ * пока на очередном проходе не окажется, что обмены больше не нужны, что означает — массив отсортирован.
+ * При каждом проходе алгоритма по внутреннему циклу очередной наибольший элемент массива ставится на
+ * своё место в конце массива рядом с предыдущим «наибольшим элементом»,
+ * а наименьший элемент перемещается на одну позицию к началу массива
+ * («всплывает» до нужной позиции, как пузырёк в воде — отсюда и название алгоритма).
+ */
+public class BubbleSort {
+    public static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{43, 67, 2, 90, 5, 10, 134, 47};
+        for (int i : a) {
+            System.out.println(i);
+        }
+        bubbleSort(a);
+        System.out.println("After sort:");
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+}
