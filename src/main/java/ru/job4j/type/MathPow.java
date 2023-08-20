a990ee7d278c0d94e28466476b142fa13994e114
D L <lazykindit@yandex.ru>
2023-08-20 18:17:18 +0300

    6. Возведения числа в степень.

---
+++ src/main/java/ru/job4j/type/MathPow.java
@@ -0,0 +1,11 @@
+package ru.job4j.type;
+
+/**
+ * Возведите число 3 в квадрат.
+ */
+public class MathPow {
+    public static void main(String[] args) {
+        double result = Math.pow(3, 2);
+        System.out.println(result);
+    }
+}
