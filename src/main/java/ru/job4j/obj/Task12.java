0d5a848f2e1985691b1a79e5010ce70daf70bc91
D L <lazykindit@yandex.ru>
2023-08-30 15:53:38 +0300

    Объявить внутренний класс

---
+++ src/main/java/ru/job4j/obj/Task12.java
@@ -0,0 +1,10 @@
+package ru.job4j.obj;
+
+/**
+ * Объявить обычный внутренний класс с именем Nested в классе Task12.
+ * Модификатор private в решении не использовать.
+ */
+public class Task12 {
+    class Nested {
+    }
+}
