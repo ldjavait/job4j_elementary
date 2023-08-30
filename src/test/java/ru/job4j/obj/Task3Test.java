9b101ef0776d3274a64040ef67591ed309417037
D L <lazykindit@yandex.ru>
2023-08-30 11:53:25 +0300

    3. Объявить класс, переменную и метод

---
+++ src/test/java/ru/job4j/obj/Task3Test.java
@@ -0,0 +1,30 @@
+package ru.job4j.obj;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.*;
+
+public class Task3Test {
+    @Test
+    public void whenCreateTask3() {
+        var task = new Task3();
+        MatcherAssert.assertThat(task, is(notNullValue()));
+    }
+
+    @Test
+    public void whenCheckVariable() {
+        var task = new Task3();
+        int x = task.x;
+        MatcherAssert.assertThat(task, is(notNullValue()));
+    }
+
+    @Test
+    public void whenMethodInvoke() {
+        var task = new Task3();
+        int x = task.getX();
+        MatcherAssert.assertThat(x, is(3));
+    }
+}
