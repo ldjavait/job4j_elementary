b38849252550d99fc6cea1ed9ed6835182100e42
D L <lazykindit@yandex.ru>
2023-09-01 17:42:03 +0300

    1. Логические выражения

---
+++ src/main/java/ru/job4j/condition/NegativeCondition.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class NegativeCondition {
+    public static void main(String[] args) {
+        int num = -1;
+        boolean result1 = true;
+        boolean result2 = false;
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
---
+++ src/test/java/ru/job4j/condition/NegativeConditionTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class NegativeConditionTest {
+    @Test
+    public void test() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        NegativeCondition.main(null);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "true" + ln + "false" + ln));
+    }
+}
