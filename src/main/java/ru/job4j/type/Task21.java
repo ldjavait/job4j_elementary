132b72de27883ecb1e209f9208829d3d3d4d4f62
D L <lazykindit@yandex.ru>
2023-09-16 09:45:56 +0300

    21. Разделить трехзначное число на отдельные цифры

---
+++ src/main/java/ru/job4j/type/Task21.java
@@ -0,0 +1,18 @@
+package ru.job4j.type;
+
+/**
+ * Отделить цифры данного числа друг от друга и вывести их в консоль в одну строку через пробел.
+ */
+public class Task21 {
+    public static void separate(int number) {
+        int num1, num2, num3;
+        num3 = number % 1000 / 100;
+        num2 = number % 100 / 10;
+        num1 = number % 10;
+        System.out.println(num3 + " " + num2 + " " + num1);
+    }
+
+    public static void main(String[] args) {
+        separate(205);
+    }
+}
