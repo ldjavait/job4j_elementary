33eed3c25acdaee804b9c6af89c7e4b023347bc0
D L <lazykindit@yandex.ru>
2023-09-16 09:41:15 +0300

    20. Разделить двузначное число на отдельные цифры

---
+++ src/main/java/ru/job4j/type/Task20.java
@@ -0,0 +1,17 @@
+package ru.job4j.type;
+
+/**
+ * Отделить цифры данного числа друг от друга и вывести их в консоль в одну строку через пробел.
+ */
+public class Task20 {
+    public static void separate(int number) {
+        int num1, num2;
+        num1 = number % 100 / 10;
+        num2 = number % 10;
+        System.out.println(num1 + " " + num2);
+    }
+
+    public static void main(String[] args) {
+        separate(20);
+    }
+}
