a670aab79fdc06756a4d077895422cc2a3c6e499
D L <lazykindit@yandex.ru>
2023-09-29 13:38:10 +0300

    3. Генерация документации по JavaDoc [#313880 #259818]. Правки по задаче.

--- src/main/java/ru/job4j/loop/MultiplicationLoop.java
+++ src/main/java/ru/job4j/loop/MultiplicationLoop.java
@@ -1,8 +1,5 @@
 package ru.job4j.loop;
 
-/**
- * Даны два целых числа a и b (a < b). Найти произведение всех целых чисел от a до b включительно.
- */
 public class MultiplicationLoop {
     public static int mult(int a, int b) {
         int rsl = 1;
