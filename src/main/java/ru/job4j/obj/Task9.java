9ca594f0cf389db33d36a0b2c330e1cfa8cd0cd6
D L <lazykindit@yandex.ru>
2023-09-29 12:17:38 +0300

    9. Объявить одностороннюю очередь

---
+++ src/main/java/ru/job4j/obj/Task9.java
@@ -0,0 +1,12 @@
+package ru.job4j.obj;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Объявить класс Task9, в нём объявить очередь queue (поле).
+ * Реализацией очереди должна быть PriorityQueue<String>.
+ */
+public class Task9 {
+    Queue<String> queue = new PriorityQueue<>();
+}
---
+++ src/test/java/ru/job4j/obj/Task9Test.java
@@ -0,0 +1,17 @@
+package ru.job4j.obj;
+
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
+import static org.junit.Assert.assertEquals;
+
+public class Task9Test {
+
+    @Test
+    public void whenCreateQueue() {
+        Task9 task = new Task9();
+        assertEquals(task.queue.getClass(), PriorityQueue.class);
+    }
+
+}
